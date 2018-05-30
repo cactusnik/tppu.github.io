dbe1eac1d2b15acecd459d6c85869cec7e28ab30
alex <alex.t@itua.com.ua>
2018-05-30 20:57:32 +0300

    add has-dropdown with js and hover

--- app/blocks/nav/nav.js
+++ app/blocks/nav/nav.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-  $('.nav__item--dropdown > a').on('click', function(){
+  var itemMenu = $('.nav__item');
+  for(var i = 0; i < itemMenu.length; i++){
+    if(itemMenu[i].children.length > 1){
+        itemMenu[i].classList.add('nav__item--dropdown');
+    }
+  }
+
+  $('.nav__item > a').on('click', function(){
     event.preventDefault();
     $(this).toggleClass('nav__item--active');
     $(this).siblings('.submenu').slideToggle(500);
