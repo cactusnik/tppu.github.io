71476828f2a5b8d38f53879b056135fc88cc3423
alex <alex.t@itua.com.ua>
2018-05-31 17:57:26 +0300

    fix js on link with not dropdown

--- app/blocks/nav/nav.js
+++ app/blocks/nav/nav.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     }
   }
 
-  $('.nav__item > a').on('click', function(){
+  $('.nav__item--dropdown > a').on('click', function(){
     event.preventDefault();
     $(this).toggleClass('nav__item--active');
     $(this).siblings('.submenu').slideToggle(500);
